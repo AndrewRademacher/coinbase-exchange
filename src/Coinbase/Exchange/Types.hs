ff71c8a9d0338f7ac6ef1521fb328a5d612beb2a
AndrewRademacher <andrewrademacher@gmail.com>
2015-04-22 17:20:28 -0500

    Fixed imports added sandbox executable.

--- src/Coinbase/Exchange/Types.hs
+++ src/Coinbase/Exchange/Types.hs
@@ -1,7 +1,11 @@
 module Coinbase.Exchange.Types where
 
-data ExchangeConf
+import           Control.Monad.Except
+import           Control.Monad.Reader
+import           Control.Monad.Trans.Resource
 
-data ExchangeFailure
+data ExchangeConf = ExchangeConf
+
+data ExchangeFailure = ExchangeFailure
 
 newtype ExchangeT m a = ExchangeT { unExchangeT :: ResourceT (ReaderT ExchangeConf (ExceptT ExchangeFailure m)) a }

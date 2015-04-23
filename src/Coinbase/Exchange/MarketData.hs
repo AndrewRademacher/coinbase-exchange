3a37e7670b4d41ff014144c239f732f934e60c1a
AndrewRademacher <andrewrademacher@gmail.com>
2015-04-23 11:41:22 -0500

    Added getCurrencies.

--- src/Coinbase/Exchange/MarketData.hs
+++ src/Coinbase/Exchange/MarketData.hs
@@ -1,19 +1,51 @@
-{-# LANGUAGE DeriveGeneric         #-}
-{-# LANGUAGE FlexibleContexts      #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE DeriveGeneric              #-}
+{-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE OverloadedStrings          #-}
 
 module Coinbase.Exchange.MarketData where
 
+import           Control.Applicative
 import           Control.Monad.Except
 import           Control.Monad.Reader
 import           Control.Monad.Trans.Resource
 import           Data.Aeson
 import           Data.Aeson.Casing
+import           Data.Scientific
+import           Data.String
+import           Data.Text                    (Text)
 import           Data.Time
 import           GHC.Generics
 
 import           Coinbase.Exchange.Rest
 import           Coinbase.Exchange.Types
+
+-- Exchange Currencies
+
+newtype CurrencyId = CurrencyId { unCurrencyId :: Text }
+    deriving (Eq, Ord, Show, Read, IsString, FromJSON, ToJSON)
+
+data Currency
+    = Currency
+        { curId      :: CurrencyId
+        , curName    :: Text
+        , curMinSize :: Scientific
+        }
+    deriving (Show, Generic)
+
+instance FromJSON Currency where
+    parseJSON (Object m)
+        = Currency <$> m .: "id"
+                   <*> m .: "name"
+                   <*> liftM read (m .: "min_size")
+    parseJSON _ = mzero
+
+getCurrencies :: (MonadResource m, MonadReader ExchangeConf m, MonadError ExchangeFailure m)
+              => m [Currency]
+getCurrencies = coinbaseRequest liveRest "/currencies"
+
+-- Exchange Time
 
 data ExchangeTime
     = ExchangeTime

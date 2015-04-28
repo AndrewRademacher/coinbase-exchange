6f2a31ff39db379e6a74a392710eb4a41513ed3d
AndrewRademacher <andrewrademacher@gmail.com>
2015-04-28 16:53:23 -0500

    Added sandbox experiment with web-sockets.

--- sbox/Main.hs
+++ sbox/Main.hs
@@ -2,16 +2,21 @@
 
 module Main where
 
+import           Control.Concurrent
+import           Control.Monad
+import           Control.Monad.IO.Class
+import           Data.Text                    (Text)
+import qualified Data.Text                    as T
+import qualified Data.Text.IO                 as T
 import           Data.Time
 import           Network.HTTP.Client
 import           Network.HTTP.Client.TLS
+import           Network.Socket               (withSocketsDo)
+import qualified Network.WebSockets           as WS
 import           System.Locale
 
 import           Coinbase.Exchange.MarketData
 import           Coinbase.Exchange.Types
-
-main :: IO ()
-main = putStrLn "Use GHCi."
 
 btc :: ProductId
 btc = "BTC-USD"
@@ -29,3 +34,23 @@ withCoinbase act = do
         case res of
             Right s -> return s
             Left  f -> error $ show f
+
+-- Socket Experimientation
+
+app :: WS.ClientApp ()
+app conn = do
+        putStrLn "Connected"
+
+        _ <- forkIO $ forever $ do
+            msg <- WS.receiveData conn
+            liftIO $ T.putStrLn msg
+
+        let loop = do
+                line <- T.getLine
+                unless (T.null line) $ WS.sendTextData conn line >> loop
+
+        loop
+        WS.sendClose conn ("Bye!" :: Text)
+
+main :: IO ()
+main = withSocketsDo $ WS.runClient "echo.websocket.org" 80 "/" app

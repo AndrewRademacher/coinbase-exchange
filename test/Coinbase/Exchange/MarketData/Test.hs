bb6bb9bd925799c63631356787f02b40b781f48a
AndrewRademacher <andrewrademacher@gmail.com>
2015-04-30 12:36:38 -0500

    Added MarketData getCurrencies parse test.

--- test/Coinbase/Exchange/MarketData/Test.hs
+++ test/Coinbase/Exchange/MarketData/Test.hs
@@ -14,11 +14,20 @@ import           Coinbase.Exchange.Types.MarketData
 
 tests :: ExchangeConf -> TestTree
 tests conf = testGroup "Market Data"
-        [ case_parseGetExchangeTime conf
+        [ case_parseGetCurrencies conf
+        , case_parseGetExchangeTime conf
         ]
 
+case_parseGetCurrencies :: ExchangeConf -> TestTree
+case_parseGetCurrencies conf = testCase "Parse get currencies." $ do
+        v <- liftIO $ runExchange conf getCurrencies
+        assertBool "Failed to parse exchange time."
+            (case v of
+                Left  _ -> False
+                Right _ -> True)
+
 case_parseGetExchangeTime :: ExchangeConf -> TestTree
-case_parseGetExchangeTime conf = testCase "Parse Exchange Time." $ do
+case_parseGetExchangeTime conf = testCase "Parse exchange time." $ do
         v <- liftIO $ runExchange conf getExchangeTime
         assertBool "Failed to parse exchange time."
             (case v of

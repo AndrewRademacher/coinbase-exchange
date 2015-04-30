a7fe221986ceede7c5ee841bf5a6292f1d5ef35d
AndrewRademacher <andrewrademacher@gmail.com>
2015-04-30 12:05:41 -0500

    Successful connection with wuss.

--- sbox/Main.hs
+++ sbox/Main.hs
@@ -6,21 +6,23 @@ import           Control.Concurrent
 import           Control.Monad
 import           Control.Monad.IO.Class
 import           Data.Aeson
-import qualified Data.ByteString              as BS
-import           Data.Text                    (Text)
-import qualified Data.Text                    as T
-import qualified Data.Text.IO                 as T
+import qualified Data.ByteString                as BS
+import           Data.Text                      (Text)
+import qualified Data.Text                      as T
+import qualified Data.Text.IO                   as T
 import           Data.Time
 import           Network.HTTP.Client
 import           Network.HTTP.Client.TLS
-import           Network.Socket               (withSocketsDo)
-import qualified Network.WebSockets           as WS
+import           Network.Socket                 (withSocketsDo)
+import qualified Network.WebSockets             as WS
 import           System.Locale
+import           Wuss
 
 import           Coinbase.Exchange.MarketData
 import           Coinbase.Exchange.Socket
 import           Coinbase.Exchange.Types
 import           Coinbase.Exchange.Types.Core
+import           Coinbase.Exchange.Types.Socket
 
 btc :: ProductId
 btc = "BTC-USD"
@@ -44,20 +46,34 @@ withCoinbase act = do
 app :: WS.ClientApp ()
 app conn = do
         putStrLn "Connected"
-
         _ <- forkIO $ forever $ do
-            msg <- WS.receiveData conn
-            liftIO $ T.putStrLn msg
+            ds <- WS.receiveData conn
+            let res = eitherDecode ds
+            print (res :: Either String ExchangeMessage)
+            -- print (decode ds :: Maybe ExchangeMessage)
+            -- msg <- WS.receiveData conn
+            -- liftIO $ T.putStrLn msg
 
-        let loop = do
-                line <- T.getLine
-                unless (T.null line) $ WS.sendTextData conn line >> loop
+        WS.sendBinaryData conn $ encode (Subscribe btc)
+        _ <- forever $ threadDelay (1000000 * 60)
+        return ()
 
-        loop
-        WS.sendClose conn ("Bye!" :: Text)
+
+-- app :: WS.ClientApp ()
+-- app conn = do
+--         putStrLn "Connected"
+--
+--         _ <- forkIO $ forever $ do
+--             msg <- WS.receiveData conn
+--             liftIO $ T.putStrLn msg
+--
+--         let loop = do
+--                 line <- T.getLine
+--                 unless (T.null line) $ WS.sendTextData conn line >> loop
+--
+--         loop
+--         WS.sendClose conn ("Bye!" :: Text)
 
 main :: IO ()
-main = withSocketsDo $ WS.runClient "echo.websocket.org" 80 "/" app
-
-msg :: BS.ByteString
-msg = "{\"type\": \"subscribe\",\"product_id\": \"BTC-USD\"}"
+main = withSocketsDo $
+    runSecureClient "ws-feed.exchange.coinbase.com" 443 "/" app

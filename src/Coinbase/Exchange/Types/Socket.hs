0ee8629516cf857b0177d5ec4a7bb88f81ed3f7b
Dimitri DeFigueiredo <defigueiredo@ucdavis.edu>
2015-10-19 23:05:25 -0600

    new JSON parsing code for web sockets
    In the middle of it. Failing tests right now. Still need
    to fix parsing of a 'funds' or 'size' field containing a
    JSON 'null'.

--- src/Coinbase/Exchange/Types/Socket.hs
+++ src/Coinbase/Exchange/Types/Socket.hs
@@ -2,11 +2,14 @@
 {-# LANGUAGE DeriveGeneric              #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE RecordWildCards            #-}
+
 
 module Coinbase.Exchange.Types.Socket where
 
 import           Control.DeepSeq
-import           Data.Aeson.Types
+import           Control.Monad
+import           Data.Aeson.Types      hiding (Error)
 import           Data.Data
 import           Data.Hashable
 import           Data.Text                    (Text)
@@ -14,60 +17,92 @@ import           Data.Time
 import           Data.Word
 import           GHC.Generics
 
-import           Coinbase.Exchange.Types.Core
+import           Coinbase.Exchange.Types.Core  hiding (OrderStatus(..))
 
 data ExchangeMessage
     = Subscribe
         { msgProductId :: ProductId
         }
-    | Received
+    | ReceivedLimit
         { msgTime      :: UTCTime
         , msgProductId :: ProductId
         , msgSequence  :: Sequence
         , msgOrderId   :: OrderId
-        , msgSize      :: Size
-        , msgPrice     :: Price
         , msgSide      :: Side
         , msgClientOid :: Maybe ClientOrderId
+        --
+        , msgPrice     :: Price
+        , msgSize      :: Size
+        }
+    | ReceivedMarket
+        { msgTime      :: UTCTime
+        , msgProductId :: ProductId
+        , msgSequence  :: Sequence
+        , msgOrderId   :: OrderId
+        , msgSide      :: Side
+        , msgClientOid :: Maybe ClientOrderId
+        -- market orders have no price and are bounded by either size, funds or both
+        , msgMarketBounds :: (Either Size (Maybe Size, Cost))
         }
     | Open
         { msgTime          :: UTCTime
         , msgProductId     :: ProductId
         , msgSequence      :: Sequence
         , msgOrderId       :: OrderId
-        , msgPrice         :: Price
-        , msgRemainingSize :: Size
         , msgSide          :: Side
-        }
-    | Done
-        { msgTime      :: UTCTime
-        , msgProductId :: ProductId
-        , msgSequence  :: Sequence
-        , msgPrice     :: Price
-        , msgOrderId   :: OrderId
-        , msgReason    :: Reason
-        , msgSide      :: Side
+        , msgRemainingSize :: Size
+        , msgPrice         :: Price
         }
     | Match
-        { msgTradeId      :: TradeId
+        { msgTime         :: UTCTime
+        , msgProductId    :: ProductId
         , msgSequence     :: Sequence
+        , msgSide         :: Side
+        , msgTradeId      :: TradeId
         , msgMakerOrderId :: OrderId
         , msgTakerOrderId :: OrderId
-        , msgTime         :: UTCTime
-        , msgProductId    :: ProductId
         , msgSize         :: Size
         , msgPrice        :: Price
-        , msgSide         :: Side
         }
-    | Change
+    | DoneLimit
         { msgTime      :: UTCTime
+        , msgProductId :: ProductId
         , msgSequence  :: Sequence
         , msgOrderId   :: OrderId
+        , msgSide      :: Side
+        , msgRemainingSize :: Size
+        , msgPrice     :: Price
+        , msgReason    :: Reason
+        , msgOrderType :: OrderType
+        }
+    | DoneMarket
+        { msgTime      :: UTCTime
         , msgProductId :: ProductId
+        , msgSequence  :: Sequence
+        , msgOrderId   :: OrderId
+        , msgSide      :: Side
+        , msgReason    :: Reason
+        , msgOrderType :: OrderType
+        }
+    | ChangeLimit
+        { msgTime      :: UTCTime
+        , msgProductId :: ProductId
+        , msgSequence  :: Sequence
+        , msgOrderId   :: OrderId
+        , msgSide      :: Side
+        , msgPrice     :: Price
         , msgNewSize   :: Size
         , msgOldSize   :: Size
-        , msgPrice     :: Price
+        }
+    | ChangeMarket
+        { msgTime      :: UTCTime
+        , msgProductId :: ProductId
+        , msgSequence  :: Sequence
+        , msgOrderId   :: OrderId
         , msgSide      :: Side
+        , msgPrice     :: Price
+        , msgNewFunds  :: Cost
+        , msgOldFunds  :: Cost
         }
     | Error
         { msgMessage :: Text
@@ -75,7 +110,213 @@ data ExchangeMessage
     deriving (Eq, Show, Read, Data, Typeable, Generic)
 
 instance NFData ExchangeMessage
-instance ToJSON ExchangeMessage where
-    toJSON = genericToJSON coinbaseAesonOptions
+
+-----------------------------
 instance FromJSON ExchangeMessage where
-    parseJSON = genericParseJSON coinbaseAesonOptions
+    parseJSON (Object m) = do
+        msgtype <- m .: "type"
+        case (msgtype :: String) of
+            "open" -> Open
+                <$> m .: "time"
+                <*> m .: "product_id"
+                <*> m .: "sequence"
+                <*> m .: "order_id"
+                <*> m .: "side"
+                <*> m .: "remaining_size"
+                <*> m .: "price"
+            "done" -> do
+                typ  <- m .: "order_type"
+                case typ of
+                    Limit -> DoneLimit
+                        <$> m .: "time"
+                        <*> m .: "product_id"
+                        <*> m .: "sequence"
+                        <*> m .: "order_id"
+                        <*> m .: "side"
+                        <*> m .: "remaining_size"
+                        <*> m .: "price"
+                        <*> m .: "reason"
+                        <*> m .: "order_type"
+                    Market -> DoneMarket
+                        <$> m .: "time"
+                        <*> m .: "product_id"
+                        <*> m .: "sequence"
+                        <*> m .: "order_id"
+                        <*> m .: "side"
+                        <*> m .: "reason"
+                        <*> m .: "order_type"
+            "match" -> Match
+                <$> m .: "time"
+                <*> m .: "product_id"
+                <*> m .: "sequence"
+                <*> m .: "side"
+                <*> m .: "trade_id"
+                <*> m .: "maker_order_id"
+                <*> m .: "taker_order_id"
+                <*> m .: "size"
+                <*> m .: "price"
+            "change" -> do
+                ms <- m .:? "new_size"
+                case (ms :: Maybe Size) of
+                    Nothing -> ChangeMarket
+                                <$> m .: "time"
+                                <*> m .: "product_id"
+                                <*> m .: "sequence"
+                                <*> m .: "order_id"
+                                <*> m .: "side"
+                                <*> m .: "price"
+                                <*> m .: "new_funds"
+                                <*> m .: "old_funds"
+                    Just _ -> ChangeLimit
+                                <$> m .: "time"
+                                <*> m .: "product_id"
+                                <*> m .: "sequence"
+                                <*> m .: "order_id"
+                                <*> m .: "side"
+                                <*> m .: "price"
+                                <*> m .: "new_size"
+                                <*> m .: "old_size"
+            "received" -> do
+                typ  <- m .:  "order_type"
+                mcid <- m .:? "client_id"
+                case typ of
+                    Limit -> ReceivedLimit
+                                <$> m .: "time"
+                                <*> m .: "product_id"
+                                <*> m .: "sequence"
+                                <*> m .: "order_id"
+                                <*> m .: "side"
+                                <*> pure (mcid :: Maybe ClientOrderId)
+                                <*> m .: "price"
+                                <*> m .: "size"
+                    Market -> ReceivedMarket
+                                <$> m .: "time"
+                                <*> m .: "product_id"
+                                <*> m .: "sequence"
+                                <*> m .: "order_id"
+                                <*> m .: "side"
+                                <*> pure mcid
+                                <*> (do
+                                        ms <- m .:? "size"
+                                        mf <- m .:? "funds"
+                                        case (ms,mf) of
+                                            (Nothing, Nothing) -> mzero
+                                            (Just s , Nothing) -> return $ Left  s
+                                            (Nothing, Just f ) -> return $ Right (Nothing, f)
+                                            (Just s , Just f ) -> return $ Right (Just s , f)
+                                            )
+
+    parseJSON _ = mzero
+---------------------------
+
+
+instance ToJSON ExchangeMessage where
+    toJSON Subscribe{..} = object
+        [ "type"       .= ("subscribe" :: Text)
+        , "product_id" .= msgProductId
+        ]
+    toJSON Open{..} = object
+        [ "type"       .= ("open" :: Text)
+        , "time"       .= msgTime
+        , "product_id" .= msgProductId
+        , "sequence"   .= msgSequence
+        , "order_id"   .= msgOrderId
+        , "side"       .= msgSide
+        , "remaining_size" .= msgRemainingSize
+        , "price"      .= msgPrice
+        ]
+    toJSON DoneLimit{..} = object
+        [ "type"       .= ("done" :: Text)
+        , "time"       .= msgTime
+        , "product_id" .= msgProductId
+        , "sequence"   .= msgSequence
+        , "order_id"   .= msgOrderId
+        , "side"       .= msgSide
+        , "remaining_size" .= msgRemainingSize
+        , "price"      .= msgPrice
+        , "reason"     .= msgReason
+        , "order_type" .= Limit
+        ]
+    toJSON DoneMarket{..} = object
+        [ "type"       .= ("done" :: Text)
+        , "time"       .= msgTime
+        , "product_id" .= msgProductId
+        , "sequence"   .= msgSequence
+        , "order_id"   .= msgOrderId
+        , "side"       .= msgSide
+        , "reason"     .= msgReason
+        , "order_type" .= Market
+        ]
+    toJSON Match{..} = object
+        [ "type"       .= ("match" :: Text)
+        , "time"       .= msgTime
+        , "product_id" .= msgProductId
+        , "sequence"   .= msgSequence
+        , "side"       .= msgSide
+        , "trade_id"   .= msgTradeId
+        , "maker_order_id" .= msgMakerOrderId
+        , "taker_order_id" .= msgTakerOrderId
+        , "size"       .= msgSize
+        , "price"      .= msgPrice
+        ]
+    toJSON Error{..} = object
+        [ "type" .= ("error" :: Text)
+        , "message" .= msgMessage
+        ]
+    toJSON ChangeLimit{..} = object
+        [ "type"       .= ("change" :: Text)
+        , "time"       .= msgTime
+        , "product_id" .= msgProductId
+        , "sequence"   .= msgSequence
+        , "order_id"   .= msgOrderId
+        , "side"       .= msgSide
+        , "new_size"   .= msgNewSize
+        , "old_size"   .= msgOldSize
+        , "price"      .= msgPrice
+        ]
+    toJSON ChangeMarket{..} = object
+        [ "type"       .= ("change" :: Text)
+        , "time"       .= msgTime
+        , "product_id" .= msgProductId
+        , "sequence"   .= msgSequence
+        , "order_id"   .= msgOrderId
+        , "side"       .= msgSide
+        , "price"      .= msgPrice
+        , "new_funds"  .= msgNewFunds
+        , "old_funds"  .= msgOldFunds
+        ]
+
+    toJSON ReceivedLimit{..} = object (
+        [ "type"       .= ("received" :: Text)
+        , "time"       .= msgTime
+        , "product_id" .= msgProductId
+        , "sequence"   .= msgSequence
+        , "order_id"   .= msgOrderId
+        , "side"       .= msgSide
+        , "size"       .= msgSize
+        , "price"      .= msgPrice
+        , "order_type" .= Limit
+        ] ++ clientID)
+            where
+                clientID = case msgClientOid of
+                    Nothing -> []
+                    Just ci -> ["client_id" .= msgClientOid ]
+
+    toJSON ReceivedMarket{..} = object (
+        ["type"       .= ("received" :: Text)
+        , "time"       .= msgTime
+        , "product_id" .= msgProductId
+        , "sequence"   .= msgSequence
+        , "order_id"   .= msgOrderId
+        , "side"       .= msgSide
+        , "order_type" .= Market
+        ] ++ clientID ++ size ++ funds)
+            where
+                clientID = case msgClientOid of
+                    Nothing -> []
+                    Just ci -> ["client_id" .= msgClientOid ]
+                (size,funds) = case msgMarketBounds of
+                    Left  s -> (["size" .= s],[])
+                    Right (ms,f) -> case ms of
+                                Nothing -> ( []            , ["funds" .= f] )
+                                Just s' -> ( ["size" .= s'], ["funds" .= f] )
--- test/Coinbase/Exchange/Socket/Test.hs
+++ test/Coinbase/Exchange/Socket/Test.hs
@@ -19,10 +19,28 @@ import           Coinbase.Exchange.Socket
 
 import qualified Coinbase.Exchange.Private.Test  as P
 
+import Debug.Trace
+
+-------------------------------------
+-- NOTE: [Connectivity Precondition]
+--
+-- The parsing tests are time-based and assume we are receiving messages during the
+-- time we are connected. However, those tests are NOT FAILSAFE.
+--
+-- ** If no data is received, parsing succeeds and, therefore, the parsing tests succeed**
+--
+-- To ensure this unsafe behavior does not go unnoticed (we thinking we are
+-- parsing correctly when, in fact, we are not parsing anything at all),
+-- We first verify we can receive at least 20 messages (i.e. a fixed minimum number)
+-- from the socket, before running the parsing tests.
+-------------------------------------
+
 
 tests :: ExchangeConf -> TestTree
 tests conf = testGroup "Socket"
-        [ testCase "Parse Socket Stream"     (parseSocket conf (threadDelay $ 1000000 * 30))
+        -- See NOTE: [Connectivity Precondition]
+        [ testCase "Do I receive messages?"  (receiveSocket conf)
+        , testCase "Parse Websocket Stream"     (parseSocket conf (threadDelay $ 1000000 * 20))
         , testCase "Parse with Market Order" (parseSocket conf (do
                                                                     threadDelay $ 1000000 * 15
                                                                     oid'<- P.run_placeOrder conf P.giveAwayOrder -- place market order
@@ -32,18 +50,27 @@ tests conf = testGroup "Socket"
         ]
 
 
+
+receiveSocket :: ExchangeConf -> IO ()
+receiveSocket conf = subscribe (apiType conf) (ProductId "BTC-USD") $ \conn -> do
+    putStr "Connected. "
+    sequence_ $ replicate 20 (receiveAndDecode conn)
+
+receiveAndDecode :: WS.Connection -> IO ()
+receiveAndDecode conn = do
+    ds <- WS.receiveData conn
+    let res = eitherDecode $ trace (show ds) ds
+    case res :: Either String ExchangeMessage of
+        Left er -> print er   >> assertFailure "Parsing failure found"
+        Right v -> {-putStr "." >> -} return ()
+
+
 -- Success: no parse errors   found while running
 -- Failure: a parse error is  found while running
 parseSocket :: ExchangeConf -> IO a -> IO ()
 parseSocket conf challenge = subscribe (apiType conf) (ProductId "BTC-USD") $ \conn -> do
     putStr "Connected. "
-    waitCancelThreads challenge $
-        forever $ do
-            ds <- WS.receiveData conn
-            let res = eitherDecode ds
-            case res :: Either String ExchangeMessage of
-                Left er -> print er   >> assertFailure "Parsing failure found"
-                Right v -> putStr "." >> return ()
+    waitCancelThreads challenge (forever $ receiveAndDecode conn)
     return ()
 
 waitCancelThreads :: IO a -> IO b -> IO (Either a b)

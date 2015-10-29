2da1f19054e45cfc00c9bbc5bf69bb647792c93f
Dimitri DeFigueiredo <defigueiredo@ucdavis.edu>
2015-10-29 14:45:19 -0600

    minor change

--- src/Coinbase/Exchange/Socket.hs
+++ src/Coinbase/Exchange/Socket.hs
@@ -14,7 +14,7 @@ import           Coinbase.Exchange.Types
 import           Coinbase.Exchange.Types.Core
 import           Coinbase.Exchange.Types.Socket
 
-subscribe :: ApiType -> ProductId -> WS.ClientApp () -> IO ()
+subscribe :: ApiType -> ProductId -> WS.ClientApp a -> IO a
 subscribe atype pid app = withSocketsDo $
         runSecureClient location 443 "/" $ \conn -> do
             WS.sendBinaryData conn $ encode (Subscribe pid)
